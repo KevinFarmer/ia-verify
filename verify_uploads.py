db8731f098c9e959a959710c0d2bd386af9fd794
Kevin Farmer <kevin.farmer96@gmail.com>
2024-03-15 07:46:34 -0400

    Updated script to pull based on uploader email

--- verify_uploads.py
+++ verify_uploads.py
@@ -1,14 +1,36 @@
+import sys
 from internetarchive import get_session, get_item, get_files
 
-# s = ArchiveSession()
+video_file_types = {'Matroska', 'WebM', 'MPEG4'}
+thumb_file_types = {'JPEG', 'WebP', 'Thumbnail'}
+info_file_types = {'JSON'}
+
+f = open('upload_issues.txt', 'w')
+
+EMAIL = ''
+
 s = get_session()
 s.mount_http_adapter()
-search_results = s.search_items('kD9NUhMg2Eg')
+search_results = s.search_items(f'uploader:{EMAIL}', fields=['identifier', 'format'])
 for result in search_results:
-    print(result['identifier'])
+    contains_video = False
+    contains_info = False
+    contains_thumb = False
+
     id = result['identifier']
-    item = get_item(id)
-    files = get_files(id)
-    print(item.metadata)
-    # print(result)
-# print(search_results)
+    files = set(result['format'])
+    if video_file_types & files:
+        contains_video = True
+    if thumb_file_types & files:
+        contains_thumb = True
+    if info_file_types & files:
+        contains_info = True
+
+    # print(result['identifier'])
+    # print(result['format'])
+    
+    if (contains_video and contains_thumb and contains_info) == False:
+        missing = 'Missing: ' + ("" if contains_video else "Video,") + ("" if contains_thumb else "Thumbnail,") + ("" if contains_info else "info.json")
+        f.write(id + '\t  [' + ', '.join(result['format']) + ']\t\t' + missing + '\n')
+        
+f.close()
