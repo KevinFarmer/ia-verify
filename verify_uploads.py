5803b8b5e02d31924550971226d1295768d10db3
Kevin Farmer <kevin.farmer96@gmail.com>
2024-03-15 08:03:31 -0400

    Add email check, generate ids file

--- verify_uploads.py
+++ verify_uploads.py
@@ -1,13 +1,17 @@
-import sys
+EMAIL = ''
+SKIP_THUMBNAILS = True
+
 from internetarchive import get_session, get_item, get_files
 
 video_file_types = {'Matroska', 'WebM', 'MPEG4'}
 thumb_file_types = {'JPEG', 'WebP', 'Thumbnail'}
 info_file_types = {'JSON'}
 
-f = open('upload_issues.txt', 'w')
+if EMAIL == '':
+    print('Please enter your email on line 1')
 
-EMAIL = ''
+f = open('upload_issues.txt', 'w')
+f2 = open('problem_ids.txt', 'w')
 
 s = get_session()
 s.mount_http_adapter()
@@ -17,11 +21,15 @@ for result in search_results:
     contains_info = False
     contains_thumb = False
 
-    id = result['identifier']
+    id: str = result['identifier']
+    # Skip uploads that aren't from tubeup
+    if not id.startswith('youtube-'):
+        continue
+
     files = set(result['format'])
     if video_file_types & files:
         contains_video = True
-    if thumb_file_types & files:
+    if (thumb_file_types & files) or SKIP_THUMBNAILS:
         contains_thumb = True
     if info_file_types & files:
         contains_info = True
@@ -33,4 +41,8 @@ for result in search_results:
         missing = 'Missing: ' + ("" if contains_video else "Video,") + ("" if contains_thumb else "Thumbnail,") + ("" if contains_info else "info.json")
         f.write(id + '\t  [' + ', '.join(result['format']) + ']\t\t' + missing + '\n')
         
+        youtube_id = id.removeprefix('youtube-')
+        youtube_url = 'https://www.youtube.com/watch?v=' + youtube_id
+        f2.write(youtube_url + '\n')
+
 f.close()
